1500a5085fc16684791ab6789d9fe6e54c7c3e90
张旭 <zhangxu@travelsky.com>
2020-12-23 18:52:14 +0800

    commit 1

--- socail relationship.py
+++ socail relationship.py
@@ -1,3 +1,9 @@
-import jieba
+import jieba   #分词系统
+import codecs  #解码器
+import pandas  #
+import numpy as np
+import image
+import jieba.posseg as pseg
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, ImageColorGenerator
 
-print(111)
